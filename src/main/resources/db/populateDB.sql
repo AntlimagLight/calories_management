0f31520b66b35711ea091c9b5bb7886118bcd753
LightAntimag <antlighter@gmail.com>
2022-10-26 09:24:05 +0300

    init and populate meals in DB.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
 DELETE
 FROM user_roles;
 DELETE
+FROM meals;
+DELETE
 FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -8,6 +10,12 @@ INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
        ('Admin', 'admin@gmail.com', 'admin'),
        ('Guest', 'guest@gmail.com', 'guest');
+
+INSERT INTO meals (user_id, description, datetime, calories)
+VALUES  (100000 ,'popcorn', 	date '2021-09-28' + time '11:00', 200),
+        (100000 ,'fish',    	date '2021-09-28' + time '15:00', 250),
+        (100001 ,'milk', 	    date '2021-09-28' + time '18:00', 100),
+        (100001 ,'sausage', 	date '2021-09-28' + time '22:00', 250);
 
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
